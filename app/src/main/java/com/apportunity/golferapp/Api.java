188ea24e4fd0da37b33c9d318a0d23941ded7efb
Iskarr <32428946+Iskarr@users.noreply.github.com>
2020-08-13 11:34:20 -0600

    updated comment again.

--- app/src/main/java/com/apportunity/golferapp/Api.java
+++ app/src/main/java/com/apportunity/golferapp/Api.java
@@ -4,7 +4,7 @@ public class Api {
 
 //    Root URL connected to the web server.
 
-    private static final String ROOT_URL = "http://ff95c1e4320d.ngrok.io/HeroApi/v1/Api.php?apicall=";
+    private static final String ROOT_URL = " http://d7b556e47ef0.ngrok.io/HeroApi/v1/Api.php?apicall=";
 
     public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
     public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
