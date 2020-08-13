09f9033c2027df036a38abbf087b4a6a2a8f8d56
Iskarr <32428946+Iskarr@users.noreply.github.com>
2020-08-13 11:03:14 -0600

    updated URL and had to repost repo.

---
+++ app/src/main/java/com/apportunity/golferapp/Api.java
@@ -0,0 +1,12 @@
+package com.apportunity.golferapp;
+
+public class Api {
+
+    private static final String ROOT_URL = "http://ff95c1e4320d.ngrok.io/HeroApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
+
+}
